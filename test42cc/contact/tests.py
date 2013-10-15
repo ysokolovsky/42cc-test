f1c75854d17a78c262685968b15811011068a71d
Yaroslav Sokolovsky <yaroslav.sokolovsky@gmail.com>
2013-10-15 12:49:21 +0300

    t1_contact: tests + model

--- test42cc/contact/tests.py
+++ test42cc/contact/tests.py
@@ -1,16 +1,17 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+from django_webtest import WebTest
+from django.core.urlresolvers import reverse
+from test_42cc.contact.models import Request
+from django.http import HttpRequest
+from django.template import RequestContext
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestContact(WebTest):
+
+    def test_t1_base(self):
+        page = self.app.get(reverse('test_42cc.contact.views.index'))
+        assert u"Name" in page
+        assert u"Last name" in page
+        assert u"Yaroslav" in page
+        assert u"Bio" in page
+        assert u"Date of birth" in page
+        assert u"1990" in page
