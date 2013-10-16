e487c4d46da2d257ced2f6cff65e481156ff9190
Yaroslav Sokolovsky <yaroslav.sokolovsky@gmail.com>
2013-10-16 12:10:18 +0300

    t3_midreq: added model

--- test42cc/contact/models.py
+++ test42cc/contact/models.py
@@ -13,3 +13,13 @@ class Contact(models.Model):
 
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
+
+
+class Request(models.Model):
+    time = models.DateTimeField(auto_now=True)
+    host = models.CharField(max_length=255)
+    path = models.CharField(max_length=255)
+    method = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return "%s - %s" % (self.host, self.path)
