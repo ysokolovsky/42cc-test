8b2b016c5978f636f3d62ae1efa5a87c02e1a868
Yaroslav Sokolovsky <yaroslav.sokolovsky@gmail.com>
2013-10-16 12:23:10 +0300

    t3_midreq: middleware + fix test

--- test42cc/contact/tests.py
+++ test42cc/contact/tests.py
@@ -1,7 +1,6 @@
 from django_webtest import WebTest
 from django.core.urlresolvers import reverse
-from django.http import HttpRequest
-from django.template import RequestContext
+from test_42cc.contact.models import Request
 
 
 class TestContact(WebTest):
