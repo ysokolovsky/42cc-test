58e50b1c71492942067bd4d046d8153f7c954c44
Yaroslav Sokolovsky <yaroslav.sokolovsky@gmail.com>
2013-10-16 22:11:25 +0300

    t3_midreq: fixed bugs

--- test42cc/contact/views.py
+++ test42cc/contact/views.py
@@ -12,6 +12,7 @@ def index(request):
 
 def show_requests(request):
     requests = Request.objects.all().order_by('time')[:10]
+    
     return render_to_response(
-        'contact/requests.html', {'requests': requests},
+        'requests/requests.html', {'requests': requests},
         context_instance=RequestContext(request))
